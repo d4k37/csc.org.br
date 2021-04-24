24df29a64aaf28755f16e0f3bf87f9f5fd015ae1
Luiz Correia <luiz@luizcorreia.eti.br>
2021-04-24 04:16:53 -0300

    :bento: feat: add images to site

--- gatsby-config.js
+++ gatsby-config.js
@@ -140,6 +140,7 @@ module.exports = {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        managementToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
